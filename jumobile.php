3e1a16468f26919d4b6f954dccd65834474dbe91
Denys <denys@joomla-ua.org>
2021-09-10 22:48:16 +0300

    Update to 1.9

--- jumobile.php
+++ jumobile.php
@@ -6,7 +6,7 @@
  * @subpackage       plg_jumobile
  *
  * @author           Denys Nosov, denys@joomla-ua.org
- * @copyright        2016-2019 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
+ * @copyright        2016-2021 (C) Joomla! Ukraine, https://joomla-ua.org. All rights reserved.
  * @license          GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -16,317 +16,326 @@
  * @license        GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
  */
 
-defined('_JEXEC') or die;
+use Joomla\CMS\Environment\Browser;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
 
-jimport('joomla.plugin.plugin');
-jimport('joomla.environment.browser');
+defined('_JEXEC') or die;
 
 require_once __DIR__ . '/lib/Mobile_Detect.php';
 
 /**
  * JUMobile Plugin.
  *
- * @property  _plugin
  * @since  1.0
  */
-class plgSystemJUMobile extends JPlugin
+class plgSystemJUMobile extends CMSPlugin
 {
-    protected $app;
-    protected $isMobile = false;
-    protected $devMode = 0;
+	protected $isMobile = false;
+	protected $devMode = 0;
 
-    /**
-     * plgSystemJUMobile constructor.
-     *
-     * @param $subject
-     * @param $params
-     */
-    public function __construct(&$subject, $params)
-    {
-        parent::__construct($subject, $params);
+	/**
+	 * @since version
+	 * @var array|mixed
+	 */
+	private $_plugin;
 
-        $this->_plugin = JPluginHelper::getPlugin('system', 'jumobile');
-        $this->_params = new JRegistry($this->_plugin->params);
-    }
+	/**
+	 * @since version
+	 * @var \Joomla\Registry\Registry
+	 */
+	private $_params;
 
-    /**
-     *
-     * @return bool
-     *
-     * @throws Exception
-     * @since 1.0
-     */
-    public function onAfterRender()
-    {
-        $app    = JFactory::getApplication();
-        $params = $this->_params;
+	/**
+	 * plgSystemJUMobile constructor.
+	 *
+	 * @param $subject
+	 * @param $params
+	 *
+	 * @since  1.0
+	 */
+	public function __construct(&$subject, $params)
+	{
+		parent::__construct($subject, $params);
 
-        if($app->getName() !== 'site')
-        {
-            return true;
-        }
+		$this->_plugin = PluginHelper::getPlugin('system', 'jumobile');
+		$this->_params = new Registry($this->_plugin->params);
+	}
 
-        if(@$_COOKIE[$this->params->get('cookiename')] == '0')
-        {
-            return true;
-        }
+	/**
+	 *
+	 * @return bool
+	 *
+	 * @throws Exception
+	 * @since 1.0
+	 */
+	public function onAfterRender()
+	{
+		$app    = Factory::getApplication();
+		$params = $this->_params;
 
-        $exclusion = $params->get('exclusion', '');
-        if($exclusion != '')
-        {
-            $urls = explode("\r\n", $exclusion);
-            foreach ($urls as $url)
-            {
-                if(strpos(JURI::current(), $url) !== false)
-                {
-                    return true;
-                }
-            }
-        }
+		if($app->getName() !== 'site')
+		{
+			return true;
+		}
 
-        $lib_md  = new Mobile_Detect();
-        $browser = JBrowser::getInstance();
-        $agent   = $browser->getAgentString();
+		if(@$_COOKIE[ $this->params->get('cookiename') ] == '0')
+		{
+			return true;
+		}
 
-        $enabled = $app->getUserStateFromRequest('jumobile.isenabled', 'jumobile', true, 'bool');
+		$exclusion = $params->get('exclusion', '');
+		if($exclusion != '')
+		{
+			$urls = explode("\r\n", $exclusion);
+			foreach($urls as $url)
+			{
+				if(strpos(JURI::current(), $url) !== false)
+				{
+					return true;
+				}
+			}
+		}
 
-        $this->isMobile = ($lib_md->isMobile() || $lib_md->isTablet());
+		$lib_md  = new Mobile_Detect();
+		$browser = Browser::getInstance();
+		$agent   = $browser->getAgentString();
 
-        if($params->get('devmode', false))
-        {
-            $ip_addresses = array_filter(explode("\r\n", $params->get('devmodeip', '')));
+		$enabled = $app->getUserStateFromRequest('jumobile.isenabled', 'jumobile', true, 'bool');
 
-            if(!empty($ip_addresses))
-            {
-                foreach ($ip_addresses as $ip)
-                {
-                    if($this->get_ip() == $ip)
-                    {
-                        $this->devMode = 1;
+		$this->isMobile = ($lib_md->isMobile() || $lib_md->isTablet());
 
-                        break;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
+		if($params->get('devmode', false))
+		{
+			$ip_addresses = array_filter(explode("\r\n", $params->get('devmodeip', '')));
 
-        if(($this->devMode == '1' ||
-                ($lib_md && $this->isMobile) ||
-                ((!$lib_md && $browser->isMobile()) || false !== stripos($agent, 'mobile'))) && $this->params->get('allowcache') == 1 && $enabled
-        )
-        {
-            $app->allowCache(true);
-            if($this->params->get('pragma') == 1)
-            {
-                $app->setHeader('Pragma', 'public', true);
-            }
+			if(!empty($ip_addresses))
+			{
+				foreach($ip_addresses as $ip)
+				{
+					if($this->get_ip() == $ip)
+					{
+						$this->devMode = 1;
 
-            if($this->params->get('cachecontrol') == 1)
-            {
-                $app->setHeader('Cache-Control', 'public', true);
-            }
+						break;
+					}
+					else
+					{
+						return true;
+					}
+				}
+			}
+		}
 
-            if($this->params->get('expires') == 1)
-            {
-                $app->setHeader('Expires', gmdate('D, d M Y H:i:s', time() + $this->params->get('expirestime')) . ' GMT', true);
-            }
-        }
+		if(($this->devMode == '1' || ($lib_md && $this->isMobile) || ((!$lib_md && $browser->isMobile()) || false !== stripos($agent, 'mobile'))) && $this->params->get('allowcache') == 1 && $enabled)
+		{
+			$app->allowCache(true);
+			if($this->params->get('pragma') == 1)
+			{
+				$app->setHeader('Pragma', 'public', true);
+			}
 
-        return true;
-    }
+			if($this->params->get('cachecontrol') == 1)
+			{
+				$app->setHeader('Cache-Control', 'public', true);
+			}
 
-    /**
-     *
-     * @return string
-     *
-     * @since 1.0
-     */
-    private function get_ip()
-    {
-        if(!empty(getenv('HTTP_CLIENT_IP')))
-        {
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-        }
-        elseif(!empty(getenv('HTTP_X_FORWARDED_FOR')))
-        {
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        }
-        elseif(!empty(getenv('HTTP_X_FORWARDED')))
-        {
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-        }
-        elseif(!empty(getenv('HTTP_FORWARDED_FOR')))
-        {
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        }
-        elseif(!empty(getenv('HTTP_FORWARDED')))
-        {
-            $ipaddress = getenv('HTTP_FORWARDED');
-        }
-        elseif(!empty(getenv('REMOTE_ADDR')))
-        {
-            $ipaddress = getenv('REMOTE_ADDR');
-        }
-        else
-        {
-            $ipaddress = 'UNKNOWN';
-        }
+			if($this->params->get('expires') == 1)
+			{
+				$app->setHeader('Expires', gmdate('D, d M Y H:i:s', time() + $this->params->get('expirestime')) . ' GMT', true);
+			}
+		}
 
-        $ips = explode(',', $ipaddress);
+		return true;
+	}
 
-        return trim($ips[0]);
-    }
+	/**
+	 *
+	 * @return string
+	 *
+	 * @since 1.0
+	 */
+	private function get_ip()
+	{
+		if(!empty(getenv('HTTP_CLIENT_IP')))
+		{
+			$ipaddress = getenv('HTTP_CLIENT_IP');
+		}
+		elseif(!empty(getenv('HTTP_X_FORWARDED_FOR')))
+		{
+			$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+		}
+		elseif(!empty(getenv('HTTP_X_FORWARDED')))
+		{
+			$ipaddress = getenv('HTTP_X_FORWARDED');
+		}
+		elseif(!empty(getenv('HTTP_FORWARDED_FOR')))
+		{
+			$ipaddress = getenv('HTTP_FORWARDED_FOR');
+		}
+		elseif(!empty(getenv('HTTP_FORWARDED')))
+		{
+			$ipaddress = getenv('HTTP_FORWARDED');
+		}
+		elseif(!empty(getenv('REMOTE_ADDR')))
+		{
+			$ipaddress = getenv('REMOTE_ADDR');
+		}
+		else
+		{
+			$ipaddress = 'UNKNOWN';
+		}
 
-    /**
-     *
-     * @return bool
-     *
-     * @throws Exception
-     * @since 1.0
-     */
-    public function onAfterInitialise()
-    {
-        $app    = JFactory::getApplication();
-        $params = $this->_params;
+		$ips = explode(',', $ipaddress);
 
-        if($app->getName() !== 'site')
-        {
-            return true;
-        }
+		return trim($ips[ 0 ]);
+	}
 
-        if(@$_COOKIE[$this->params->get('cookiename')] == '0')
-        {
-            return true;
-        }
+	/**
+	 *
+	 * @return bool
+	 *
+	 * @throws Exception
+	 * @since 1.0
+	 */
+	public function onAfterInitialise()
+	{
+		$app    = Factory::getApplication();
+		$params = $this->_params;
 
-        $exclusion = $params->get('exclusion', '');
-        if($exclusion != '')
-        {
-            $urls = explode("\r\n", $exclusion);
-            foreach ($urls as $url)
-            {
-                if(strpos(JURI::current(), $url) !== false)
-                {
-                    return true;
-                }
-            }
-        }
+		if($app->getName() !== 'site')
+		{
+			return true;
+		}
 
-        $lib_md  = new Mobile_Detect();
-        $browser = JBrowser::getInstance();
-        $agent   = $browser->getAgentString();
+		if(@$_COOKIE[ $this->params->get('cookiename') ] == '0')
+		{
+			return true;
+		}
 
-        $enabled = $app->getUserStateFromRequest('jumobile.isenabled', 'jumobile', true, 'bool');
+		$exclusion = $params->get('exclusion', '');
+		if($exclusion != '')
+		{
+			$urls = explode("\r\n", $exclusion);
+			foreach($urls as $url)
+			{
+				if(strpos(JURI::current(), $url) !== false)
+				{
+					return true;
+				}
+			}
+		}
 
-        $this->isMobile = ($lib_md->isMobile() || $lib_md->isTablet());
+		$lib_md  = new Mobile_Detect();
+		$browser = Browser::getInstance();
+		$agent   = $browser->getAgentString();
 
-        if($params->get('devmode', false))
-        {
-            $ip_addresses = array_filter(explode("\r\n", $params->get('devmodeip', '')));
+		$enabled = $app->getUserStateFromRequest('jumobile.isenabled', 'jumobile', true, 'bool');
 
-            if(!empty($ip_addresses))
-            {
-                foreach ($ip_addresses as $ip)
-                {
-                    if($this->get_ip() == $ip)
-                    {
-                        $this->devMode = 1;
-                        $lib_md        = 1;
-                        $app->setUserState('jumobile.ismobile', true);
-                        $app->setUserState('jumobile.device', 'mobile');
+		$this->isMobile = ($lib_md->isMobile() || $lib_md->isTablet());
 
-                        break;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
+		if($params->get('devmode', false))
+		{
+			$ip_addresses = array_filter(explode("\r\n", $params->get('devmodeip', '')));
 
-        if($this->devMode == '1' ||
-            ($lib_md && $this->isMobile) ||
-            ((!$lib_md && $browser->isMobile()) || false !== stripos($agent, 'mobile')) // stristr($agent, 'mobile')
-        )
-        {
-            if($enabled)
-            {
-                $redirect     = $params->get('redirectmobile', false);
-                $redirectPage = $params->get('redirectpage', false);
-                $redirectOnce = $params->get('redirectonce', false);
-                $mobiledomain = $params->get('mobiledomain', false);
+			if(!empty($ip_addresses))
+			{
+				foreach($ip_addresses as $ip)
+				{
+					if($this->get_ip() == $ip)
+					{
+						$this->devMode = 1;
+						$lib_md        = 1;
+						$app->setUserState('jumobile.ismobile', true);
+						$app->setUserState('jumobile.device', 'mobile');
 
-                $template = $params->get('template', false);
+						break;
+					}
+					else
+					{
+						return true;
+					}
+				}
+			}
+		}
 
-                if($redirect &&
-                    (!$app->getUserState('jumobile.isredirected', false) || !$redirectOnce)
-                )
-                {
-                    $uri = JUri::getInstance();
-                    if(substr($uri->current(), 0, strlen($mobiledomain)) != $mobiledomain)
-                    {
-                        $app->setUserState('jumobile.isredirected', true);
-                        $page = ltrim($uri->toString(array('path', 'query')), '/');
+		if($this->devMode == '1' || ($lib_md && $this->isMobile) || ((!$lib_md && $browser->isMobile()) || false !== stripos($agent, 'mobile')) // stristr($agent, 'mobile')
+		)
+		{
+			if($enabled)
+			{
+				$redirect     = $params->get('redirectmobile', false);
+				$redirectPage = $params->get('redirectpage', false);
+				$redirectOnce = $params->get('redirectonce', false);
+				$mobiledomain = $params->get('mobiledomain', false);
 
-                        if(preg_match('#^http(?:s)?\:\/\/.*#i', $mobiledomain))
-                        {
-                            $app->redirect($mobiledomain . ($redirectPage ? '/' . $page : ''));
-                        }
-                        else
-                        {
-                            if($uri->getHost() != $mobiledomain)
-                            {
-                                $url = $uri->getScheme() . '://' . $mobiledomain;
-                                $app->redirect($url . ($redirectPage ? '/' . $page : ''));
-                            }
-                        }
-                    }
-                }
+				$template = $params->get('template', false);
 
-                if($template > 0 && $template != '-1')
-                {
-                    $app->input->set('templateStyle', $template);
-                }
+				if($redirect && (!$app->getUserState('jumobile.isredirected', false) || !$redirectOnce))
+				{
+					$uri = Uri::getInstance();
+					if(substr($uri->current(), 0, strlen($mobiledomain)) != $mobiledomain)
+					{
+						$app->setUserState('jumobile.isredirected', true);
+						$page = ltrim($uri->toString([ 'path', 'query' ]), '/');
 
-                $app->setUserState('jumobile.ismobile', true);
-            }
-            else
-            {
-                $app->setUserState('jumobile.ismobile', false);
-            }
+						if(preg_match('#^http(?:s)?\:\/\/.*#i', $mobiledomain))
+						{
+							$app->redirect($mobiledomain . ($redirectPage ? '/' . $page : ''));
+						}
+						else
+						{
+							if($uri->getHost() != $mobiledomain)
+							{
+								$url = $uri->getScheme() . '://' . $mobiledomain;
+								$app->redirect($url . ($redirectPage ? '/' . $page : ''));
+							}
+						}
+					}
+				}
 
-            $device = new JRegistry();
-            $device->set('browser', $browser->getBrowser());
-            $device->set('platform', $browser->getPlatform());
+				if($template > 0 && $template != '-1')
+				{
+					$app->input->set('templateStyle', $template);
+				}
 
-            $app->setUserState('jumobile.isdevice', true);
-            $app->setUserState('jumobile.device', $device->toString());
-            $app->setUserState('jumobile.detection.system', $lib_md);
-            $app->input->def('jumobile', true);
-        }
-        else
-        {
-            $app->setUserState('jumobile.ismobile', false);
-            $app->setUserState('jumobile.isdevice', false);
-            $app->setUserState('jumobile.device', '{}');
-            $app->setUserState('jumobile.detection.system', false);
-            $app->input->def('jumobile', false);
-        }
+				$app->setUserState('jumobile.ismobile', true);
+			}
+			else
+			{
+				$app->setUserState('jumobile.ismobile', false);
+			}
 
-        $cachingOn = true;
+			$device = new Registry();
+			$device->set('browser', $browser->getBrowser());
+			$device->set('platform', $browser->getPlatform());
 
-        if($cachingOn)
-        {
-            $registeredurlparams           = !empty($app->registeredurlparams) ? $app->registeredurlparams : new stdClass;
-            $registeredurlparams->jumobile = 'BOOLEAN';
-            $app->registeredurlparams      = $registeredurlparams;
-        }
+			$app->setUserState('jumobile.isdevice', true);
+			$app->setUserState('jumobile.device', $device->toString());
+			$app->setUserState('jumobile.detection.system', $lib_md);
+			$app->input->def('jumobile', true);
+		}
+		else
+		{
+			$app->setUserState('jumobile.ismobile', false);
+			$app->setUserState('jumobile.isdevice', false);
+			$app->setUserState('jumobile.device', '{}');
+			$app->setUserState('jumobile.detection.system', false);
+			$app->input->def('jumobile', false);
+		}
 
-        return true;
-    }
+		$cachingOn = true;
+
+		if($cachingOn)
+		{
+			$registeredurlparams           = !empty($app->registeredurlparams) ? $app->registeredurlparams : new stdClass;
+			$registeredurlparams->jumobile = 'BOOLEAN';
+			$app->registeredurlparams      = $registeredurlparams;
+		}
+
+		return true;
+	}
 }
